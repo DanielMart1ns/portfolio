bbb9c234a5847f5e6902beab8a0018d5e47178a3
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-03-25 15:00:34 -0300

    feat: create structure and section about me

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/DanielMart1ns.png" alt="" />
+    <Titulo fontSize={20}>Daniel Martins</Titulo>
+  </aside>
+)
+
+export default Sidebar
